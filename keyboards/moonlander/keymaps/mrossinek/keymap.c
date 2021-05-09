6d81ed0d5c39f1c0c38f4838ef59876a561da616
Max Rossmannek <max.rossmannek@uzh.ch>
2021-05-09 23:24:36 +0200

    RAlt is my Mode_switch key in XModmap

--- keyboards/moonlander/keymaps/mrossinek/keymap.c
+++ keyboards/moonlander/keymaps/mrossinek/keymap.c
@@ -102,12 +102,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+--------+--------+--------+--------+--------+--------|                                          |--------+--------+--------+--------+--------+--------+--------|
  * |    `   |    Q   |    W   |    E   |    R   |    T   |  Del   |                                          | Insert |    Y   |    U   |    I   |    O   |    P   |   '    |
  * |--------+--------+--------+--------+--------+--------+--------|                                          |--------+--------+--------+--------+--------+--------+--------|
- * |   Tab  |    A   |    S   |    D   |    F   |    G   | PgDown |                                          |  PgUp  |    H   |    J   |    K   |    L   |    ;   |   \    |
+ * |   Tab  |    A   |    S   |    D   |    F   |    G   | PgDown |                                          |  PgUp  |    H   |    J   |    K   |    L   |    ;   | \/RAlt |
  * |--------+--------+--------+--------+--------+--------+--------'                                          `--------+--------+--------+--------+--------+--------+--------|
  * | LShift |    Z   |    X   |    C   |    V   |    B   |                                                            |    N   |    M   |    ,   |    .   |    /   | RShift |
  * |--------+--------+--------+--------+--------+--------'                                                            '--------+--------+--------+--------+--------+--------|
  * | LCtrl  |    [   |    ]   |  Left  |  Right |         ,-----------------.                      ,-----------------.         |   Up   |  Down  |    -   |    =   | RCtrl  |
- * `--------------------------------------------'         |       L2        |                     |       L4        |         `--------------------------------------------'
+ * `--------------------------------------------'         |       L2        |                      |       L4        |         `--------------------------------------------'
  *                                                        |--------+-----------------.    ,--------+-----------------|
  *                                                        |        |        |        |    |        |        |        |
  *                                                        | Space  | Back-  |  LGUI  |    |  LAlt  |  Esc   | Enter  |
@@ -118,7 +118,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [BASE] = LAYOUT_moonlander(
      TG(MDIA), KC_1   , KC_2   , KC_3   , KC_4  ,  KC_5   , KC_HYPR,                                         KC_MEH , KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_PSCR ,
      KC_GRV  , KC_Q   , KC_W   , KC_E   , KC_R  ,  KC_T   , KC_DEL ,                                         KC_INS , KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_QUOT ,
-     KC_TAB  , KC_A   , KC_S   , KC_D   , KC_F  ,  KC_G   , KC_PGDN,                                         KC_PGUP, KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_BSLS ,
+     KC_TAB  , KC_A   , KC_S   , KC_D   , KC_F  ,  KC_G   , KC_PGDN,                                         KC_PGUP, KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, RALT_T(KC_BSLS),
      KC_LSFT , KC_Z   , KC_X   , KC_C   , KC_V  ,  KC_B   ,                                                           KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_RSFT ,
      KC_LCTRL, KC_LBRC, KC_RBRC, KC_LEFT, KC_RGHT,          OSL(SYMB)       ,                       OSL(UNIC)     ,            KC_UP  , KC_DOWN, KC_MINS, KC_EQL , KC_RCTRL,
                                                             KC_SPC , KC_BSPC, KC_LGUI,     KC_LALT, KC_ESC, KC_ENT
